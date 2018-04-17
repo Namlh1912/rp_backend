d2ec0dc3e6e9dda427d422100ec35a4478396f60
Namlh1912 <namlh1912@gmail.com>
2018-04-17 21:19:06 +0700

    Update rate csv

--- api/repositories/RateRepository.js
+++ api/repositories/RateRepository.js
@@ -1,41 +1,46 @@
-const Bluebird = require('bluebird-global');
+const Bluebird = require("bluebird-global");
 
 class RateRepository {
+  constructor() {
+    this._RateModel = Bluebird.promisifyAll(Rate);
+  }
 
-	constructor() {
-		this._RateModel = Bluebird.promisifyAll(Rate);
-	}
+  create(data) {
+    return Rate.create(data);
+  }
 
-	create(data) {
-		return Rate.create(data);
-	}
-
-	getList() {
-		return this._RateModel.queryAsync(`
-			select p.name as product, c.name as customer, r.rating as rate
+  getList() {
+    return this._RateModel.queryAsync(
+      `
+			select p.name as product, c.name as customer,c.email as email , c.phone as phone ,  r.rating as rate
 			from rates r
 			left outer join products p on r.productId = p.id
 			left outer join customers c on r.customerId = c.id
-		`, []);
-	}
+		`,
+      []
+    );
+  }
 
-	getDetail(id) {
-		return Rate.findOne({
-			id
-		});
-	}
+  getDetail(id) {
+    return Rate.findOne({
+      id
+    });
+  }
 
-	remove(id) {
-		return this._RateModel.destroyAsync({
-			id: id
-		});
-	}
+  remove(id) {
+    return this._RateModel.destroyAsync({
+      id: id
+    });
+  }
 
-	update(data) {
-		return Rate.update({
-			id: data.id
-		}, data);
-	}
+  update(data) {
+    return Rate.update(
+      {
+        id: data.id
+      },
+      data
+    );
+  }
 }
 
 module.exports = RateRepository;
